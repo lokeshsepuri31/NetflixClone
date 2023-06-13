99c09014ec5e232eec22b7cad3330916cdbe2f57
saikiranjonnada <96418926+saikiranjonnada@users.noreply.github.com>
2023-06-13 16:55:56 +0530

    Changed the interceptors with headers and removed them for the interfaces

--- app/src/main/java/com/example/netflix/data/MoviesInterface.java
+++ app/src/main/java/com/example/netflix/data/MoviesInterface.java
@@ -9,19 +9,15 @@ import retrofit2.http.Path;
 
 public interface MoviesInterface {
 
-    @Headers({"X-RapidAPI-Key:b9dc4e53d0msh5ec9d0aa590aa00p17a594jsn7c891b7f6507","X-RapidAPI-Host:moviesdatabase.p.rapidapi.com"})
     @GET("/titles")
     Call<Titles> getMovies();
 
-    @Headers({"X-RapidAPI-Key:b9dc4e53d0msh5ec9d0aa590aa00p17a594jsn7c891b7f6507","X-RapidAPI-Host:moviesdatabase.p.rapidapi.com"})
     @GET("/titles/x/upcoming")
     Call<Titles> getUpcomingMovies();
 
-    @Headers({"X-RapidAPI-Key:b9dc4e53d0msh5ec9d0aa590aa00p17a594jsn7c891b7f6507","X-RapidAPI-Host:moviesdatabase.p.rapidapi.com"})
     @GET("/titles/x/upcoming")
     Call<Titles> getSeries();
 
-    @Headers({"X-RapidAPI-Key:b9dc4e53d0msh5ec9d0aa590aa00p17a594jsn7c891b7f6507","X-RapidAPI-Host:moviesdatabase.p.rapidapi.com"})
     @GET("/titles/search/akas/{aka}")
     Call<Titles> getMoviesByTitle(@Path("aka") String titleName);
 }
--- app/src/main/java/com/example/netflix/util/RetrofitClient.java
+++ app/src/main/java/com/example/netflix/util/RetrofitClient.java
@@ -1,6 +1,11 @@
 package com.example.netflix.util;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,11 +14,16 @@ public class RetrofitClient {
 
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient() {
+    private static final String MOVIES_API_VALUE = "b9dc4e53d0msh5ec9d0aa590aa00p17a594jsn7c891b7f6507";
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+    public static Retrofit getClient() {
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor(){
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request().newBuilder().addHeader("X-RapidAPI-Key", MOVIES_API_VALUE).build();
+                return chain.proceed(request);
+            }
+        }).build();
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://moviesdatabase.p.rapidapi.com/")
